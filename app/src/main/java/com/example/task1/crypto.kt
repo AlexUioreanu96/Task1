c2526b0d6e1df8bdf78be6f3c53b416bf00a32a9
alexuioreanu <alexandru.uioreanu@yopeso.com>
2022-07-20 14:52:25 +0300

    Finishing the layout and making the data clas model

---
+++ app/src/main/java/com/example/task1/crypto.kt
@@ -0,0 +1,12 @@
+package com.example.task1
+
+data class crypto(
+    val id: Int,
+    val name: String,
+    val symbol: String,
+    val rank: Int,
+    val is_new: Boolean,
+    val is_active: Boolean,
+    val type: String
+) {
+}

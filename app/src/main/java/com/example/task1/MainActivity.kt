996ecb87f4fc9b88e21c243062c2700139b659c8
alexuioreanu <alexandru.uioreanu@yopeso.com>
2022-07-20 15:29:36 +0300

    Creating a method to deserelize the file, a lot of googleing

--- app/src/main/java/com/example/task1/MainActivity.kt
+++ app/src/main/java/com/example/task1/MainActivity.kt
@@ -1,18 +1,34 @@
 package com.example.task1
 
+import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import java.io.Reader
-import java.nio.file.Files
-import java.nio.file.Paths
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var reader: Reader = Files.newBufferedReader(Paths.get("res/raw/list.json"))
-
 
     }
+
+
+    fun getList(context: Context): List<crypto> {
+
+        lateinit var jsonString: String
+        try {
+            jsonString = context.assets.open("assets/list2.json")
+                .bufferedReader()
+                .use { it.readText() }
+        } catch (ioException: IOException) {
+            println("///...///")
+        }
+
+        val listCountryType = object : TypeToken<List<crypto>>() {}.type
+        return Gson().fromJson(jsonString, listCountryType)
+    }
+
 }

4260ff242e76aeb4461eed7942271b6b0ae6bd71
alexuioreanu <alexandru.uioreanu@yopeso.com>
2022-07-20 16:05:57 +0300

    Doing continuu google, doing more debugs , some small config and problems for reading and enjoy

--- app/src/main/java/com/example/task1/MainActivity.kt
+++ app/src/main/java/com/example/task1/MainActivity.kt
@@ -2,33 +2,53 @@ package com.example.task1
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.example.task1.databinding.ActivityMainBinding
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import java.io.IOException
 
+const val TAG = "MainActivity"
+
+
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
 
+        val list = getList(this)
+
+        println(list)
+        for (i in list) {
+            println(i.toString())
+        }
+
+        if (list.isNotEmpty()) {
+            binding.txt1.setText(list[0].toString())
+            binding.txt2.setText(list[1].toString())
+            binding.txt3.setText(list[2].toString())
+        }
     }
 
 
     fun getList(context: Context): List<crypto> {
-
         lateinit var jsonString: String
         try {
-            jsonString = context.assets.open("assets/list2.json")
+            jsonString = context.resources.openRawResource(R.raw.list2)
                 .bufferedReader()
                 .use { it.readText() }
         } catch (ioException: IOException) {
-            println("///...///")
+            Log.w(TAG, "bad reading")
+            return emptyList()
         }
 
         val listCountryType = object : TypeToken<List<crypto>>() {}.type
         return Gson().fromJson(jsonString, listCountryType)
     }
-
 }
--- app/src/main/java/com/example/task1/crypto.kt
+++ app/src/main/java/com/example/task1/crypto.kt
@@ -1,11 +1,16 @@
 package com.example.task1
 
 data class crypto(
-    val id: Int,
+    val id: String,
     val name: String,
     val symbol: String,
     val rank: Int,
     val is_new: Boolean,
     val is_active: Boolean,
     val type: String
-)
+) {
+    override fun toString(): String {
+        return "crypto(id='$id', name='$name', symbol='$symbol', rank=$rank, is_new=$is_new, is_active=$is_active, type='$type')"
+    }
+}
+

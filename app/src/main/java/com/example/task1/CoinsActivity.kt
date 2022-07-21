eca826013531805e91adf50c5305692a657c9889
alexuioreanu <alexandru.uioreanu@yopeso.com>
2022-07-21 11:45:35 +0300

    Creating the clickble effect on the card, added in the style directly, i created the helper class named "FileUtil". creating the POJO from the jason, doing some debugs

--- app/src/main/java/com/example/task1/CoinsActivity.kt
+++ app/src/main/java/com/example/task1/CoinsActivity.kt
@@ -1,13 +1,8 @@
 package com.example.task1
 
-import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.task1.databinding.ActivityCoinsBinding
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
-import java.io.IOException
 
 const val TAG = "MainActivity"
 
@@ -22,28 +17,13 @@ class CoinsActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
-        val list = getList(this)
+        val util = FileUtils();
+        val list = util.getList(this, R.raw.list2)
 
         if (list.isNotEmpty()) {
             binding.txt1.text = list[0].toString()
             binding.txt2.text = list[1].toString()
             binding.txt3.text = list[2].toString()
         }
-    }
-
-
-    private fun getList(context: Context): List<CoinModel> {
-        lateinit var jsonString: String
-        try {
-            jsonString = context.resources.openRawResource(R.raw.list2)
-                .bufferedReader()
-                .use { it.readText() }
-        } catch (ioException: IOException) {
-            Log.w(TAG, "bad reading")
-            return emptyList()
-        }
-
-        val listCountryType = object : TypeToken<List<CoinModel>>() {}.type
-        return Gson().fromJson(jsonString, listCountryType)
     }
 }
---
+++ app/src/main/java/com/example/task1/FileUtils.kt
@@ -0,0 +1,27 @@
+package com.example.task1
+
+import android.content.Context
+import android.util.Log
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import java.io.IOException
+
+class FileUtils {
+
+
+    fun getList(context: Context, id: Int): List<CoinModel> {
+        lateinit var jsonString: String
+        try {
+            jsonString = context.resources.openRawResource(id)
+                .bufferedReader()
+                .use { it.readText() }
+        } catch (ioException: IOException) {
+            Log.w(TAG, "bad reading")
+            return emptyList()
+        }
+
+        val listCountryType = object : TypeToken<List<CoinModel>>() {}.type
+        return Gson().fromJson(jsonString, listCountryType)
+    }
+
+}

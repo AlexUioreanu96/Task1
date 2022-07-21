c4c3bf77cb19dd5eb6d494cdd6df4da1973d4637
alexuioreanu <alexandru.uioreanu@yopeso.com>
2022-07-21 12:32:04 +0300

    I forgot to add them in my last commit

---
+++ app/src/main/java/com/example/task1/CoinDetailsActivity.kt
@@ -0,0 +1,21 @@
+package com.example.task1
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.task1.databinding.ActivityCoinDetailsBinding
+
+class CoinDetailsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCoinDetailsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityCoinDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        
+    }
+}

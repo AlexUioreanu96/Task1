96a5cdf5da5b573d6bc749be5b8ff2b68797ecd6
alexuioreanu <alexandru.uioreanu@yopeso.com>
2022-07-21 13:38:07 +0300

    Adding a property to LinksExtended

--- app/src/main/java/com/example/task1/models/LinksExtended.kt
+++ app/src/main/java/com/example/task1/models/LinksExtended.kt
@@ -6,7 +6,9 @@ import com.google.gson.annotations.Expose
 data class LinksExtended(
     @Expose
     var type: String? = "",
-
     @Expose
-    var url: String? = ""
+    var url: String? = "",
+
+    var stats: StatsModel
+
 )

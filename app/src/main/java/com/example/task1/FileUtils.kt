38706190ceefeb927c6af707b84b7e1c2cf417b7
alexuioreanu <alexandru.uioreanu@yopeso.com>
2022-07-21 15:20:31 +0300

    Creating a method in FileUtils to read the file and get the obj deserialize

--- app/src/main/java/com/example/task1/FileUtils.kt
+++ app/src/main/java/com/example/task1/FileUtils.kt
@@ -2,6 +2,7 @@ package com.example.task1
 
 import android.content.Context
 import android.util.Log
+import com.example.task1.models.CoinDetailsDTO
 import com.example.task1.models.CoinModel
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
@@ -24,6 +25,22 @@ class FileUtils {
             val listCountryType = object : TypeToken<List<CoinModel>>() {}.type
             return Gson().fromJson(jsonString, listCountryType)
         }
+
+        fun getObj(context: Context, id: Int): CoinDetailsDTO {
+            lateinit var jsonString: String
+            try {
+                jsonString = context.resources.openRawResource(id)
+                    .bufferedReader()
+                    .use { it.readText() }
+            } catch (ioException: IOException) {
+                Log.w(TAG, "bad reading")
+                return CoinDetailsDTO()
+            }
+
+            val listCountryType = object : TypeToken<CoinDetailsDTO>() {}.type
+            return Gson().fromJson(jsonString, listCountryType)
+        }
     }
+
 
 }

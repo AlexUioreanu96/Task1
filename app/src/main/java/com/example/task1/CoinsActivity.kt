4ba99b74a5f7ad1e6daa568bcd6a375c333fa1c0
alexuioreanu <alexandru.uioreanu@yopeso.com>
2022-07-22 09:09:21 +0300

    Adding some elements on the main layput and some logic, doing some layout and logic improvments..

--- app/src/main/java/com/example/task1/CoinsActivity.kt
+++ app/src/main/java/com/example/task1/CoinsActivity.kt
@@ -1,6 +1,6 @@
 package com.example.task1
 
-import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.task1.FileUtils.Companion.getList
@@ -23,28 +23,61 @@ class CoinsActivity : AppCompatActivity() {
 
 
         if (list.isNotEmpty()) {
-            binding.txt1.text = list[0].toString()
-            binding.txt2.text = list[1].toString()
-            binding.txt3.text = list[2].toString()
-        }
+            binding.apply {
+                name1.text = list[0].name
+                symbol1.text = list[0].symbol
+                if (list[0].is_active) active1.apply {
+                    text = "active"
+                    setTextColor(Color.GREEN)
+                } else {
+                    active1.apply {
+                        text = "unactive"
+                        setTextColor(Color.RED)
+                    }
+                }
+                name2.text = list[1].name
+                symbol2.text = list[1].symbol
+                if (list[1].is_active) active2.apply {
+                    text = "active"
+                    setTextColor(Color.GREEN)
+                } else {
+                    active2.apply {
+                        text = "unactive"
+                        setTextColor(Color.RED)
+                    }
+                }
 
-        binding.txt1.setOnClickListener {
-            Intent(this, CoinDetailsActivity::class.java).also {
-                it.putExtra(ID, list[0].id)
-                startActivity(it)
+                name3.text = list[2].name
+                symbol3.text = list[2].symbol
+                if (list[2].is_active) active3.apply {
+                    text = "active"
+                    setTextColor(Color.GREEN)
+                } else {
+                    active3.apply {
+                        text = "unactive"
+                        setTextColor(Color.RED)
+                    }
+                }
             }
         }
-        binding.txt2.setOnClickListener {
-            Intent(this, CoinDetailsActivity::class.java).also {
-                it.putExtra(ID, list[1].id)
-                startActivity(it)
-            }
-        }
-        binding.txt3.setOnClickListener {
-            Intent(this, CoinDetailsActivity::class.java).also {
-                it.putExtra(ID, list[2].id)
-                startActivity(it)
-            }
-        }
+//
+//        binding.txt1.setOnClickListener {
+//            Intent(this, CoinDetailsActivity::class.java).also {
+//                it.putExtra(ID, list[0].id)
+//                startActivity(it)
+//            }
+//        }
+//        binding.txt2.setOnClickListener {
+//            Intent(this, CoinDetailsActivity::class.java).also {
+//                it.putExtra(ID, list[1].id)
+//                startActivity(it)
+//            }
+//        }
+//        binding.txt3.setOnClickListener {
+//            Intent(this, CoinDetailsActivity::class.java).also {
+//                it.putExtra(ID, list[2].id)
+//                startActivity(it)
+//            }
+//        }
     }
 }

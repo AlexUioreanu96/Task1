5c12757318b69881ea5da51e320a29185f77899b
alexuioreanu <alexandru.uioreanu@yopeso.com>
2022-07-31 17:41:37 +0300

    Test the adapter and it all works :D

--- app/src/main/java/com/example/task1/CoinDetailsActivity.kt
+++ app/src/main/java/com/example/task1/CoinDetailsActivity.kt
@@ -3,6 +3,8 @@ package com.example.task1
 import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.task1.adapter.CoinTeamAdapter
 import com.example.task1.adapter.GridAdapter
 import com.example.task1.adapter.ID
 import com.example.task1.databinding.ActivityCoinDetailsBinding
@@ -12,6 +14,7 @@ import com.example.task1.models.CoinDetailsDTO
 class CoinDetailsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityCoinDetailsBinding
+    private lateinit var teamAdapter: CoinTeamAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,14 +22,23 @@ class CoinDetailsActivity : AppCompatActivity() {
         binding = ActivityCoinDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
         supportActionBar?.hide()
+
 
         var extras: String? = intent.getStringExtra(ID)
 
         extras = extras?.replace('-', '_')
 
         val coin = extras?.let { FileUtils.getObj(this, it) }
+
+        if (coin?.team?.isNotEmpty() == true) {
+            teamAdapter = CoinTeamAdapter(coin.team)
+            binding.teamList.apply {
+                layoutManager =
+                    GridLayoutManager(applicationContext, 2, horizontalFadingEdgeLength, false)
+                adapter = teamAdapter
+            }
+        }
 
         populateView(coin)
 

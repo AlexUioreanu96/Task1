b6ff3d31a5a59cf71dc5b0a72603160777cd1b27
alexuioreanu <alexandru.uioreanu@yopeso.com>
2022-07-31 14:40:47 +0300

    Finishing the onCreateViewHolder in CoinAdapter

--- app/src/main/java/com/example/task1/adapter/CoinAdapter.kt
+++ app/src/main/java/com/example/task1/adapter/CoinAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.task1.adapter
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.task1.databinding.ItemCoinBinding
@@ -9,7 +10,13 @@ class CoinAdapter : RecyclerView.Adapter<CoinAdapter.CoinViewHolder>() {
     data class CoinViewHolder(val bind: ItemCoinBinding) : RecyclerView.ViewHolder(bind.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CoinViewHolder {
-        TODO("Not yet implemented")
+        return CoinViewHolder(
+            ItemCoinBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
     }
 
     override fun onBindViewHolder(holder: CoinViewHolder, position: Int) {

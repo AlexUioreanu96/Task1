4373fae99bbfeccf68adbd6017692ba26d05570f
alexuioreanu <alexandru.uioreanu@yopeso.com>
2022-07-31 15:07:37 +0300

    Finishing onBind method

--- app/src/main/java/com/example/task1/adapter/CoinAdapter.kt
+++ app/src/main/java/com/example/task1/adapter/CoinAdapter.kt
@@ -1,11 +1,19 @@
 package com.example.task1.adapter
 
+import android.content.Intent
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.task1.CoinDetailsActivity
 import com.example.task1.databinding.ItemCoinBinding
+import com.example.task1.models.CoinDetailsDTO
 
-class CoinAdapter : RecyclerView.Adapter<CoinAdapter.CoinViewHolder>() {
+const val ID = "ID"
+
+class CoinAdapter(var coinList: List<CoinDetailsDTO>) :
+    RecyclerView.Adapter<CoinAdapter.CoinViewHolder>() {
+
 
     data class CoinViewHolder(val bind: ItemCoinBinding) : RecyclerView.ViewHolder(bind.root)
 
@@ -20,7 +28,22 @@ class CoinAdapter : RecyclerView.Adapter<CoinAdapter.CoinViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: CoinViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val coin: CoinDetailsDTO = coinList[position]
+        holder.bind.apply {
+            cName.text = coin.name
+            cSymbol.text = coin.symbol
+            cRank.text = coin.rank.toString()
+            cName.text = coin.name
+            cType.text = coin.type
+            if (coin.isNew) cIsNew.setTextColor(Color.GREEN) else cIsNew.setTextColor(Color.RED)
+            if (coin.isActive) cIsActive.setTextColor(Color.GREEN) else cIsActive.setTextColor(Color.RED)
+            itemCoin.setOnClickListener {
+                Intent(it.context, CoinDetailsActivity::class.java).apply {
+                    putExtra(ID, coin.id)
+                    it.context.startActivity(this)
+                }
+            }
+        }
     }
 
     override fun getItemCount(): Int {

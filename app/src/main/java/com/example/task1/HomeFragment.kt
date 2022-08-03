aa90e94c538c6e63707b060fb4e6b99879a7e7f2
alexuioreanu <alexandru.uioreanu@yopeso.com>
2022-08-03 12:34:16 +0300

    Finishing the transaction between fragments

--- app/src/main/java/com/example/task1/HomeFragment.kt
+++ app/src/main/java/com/example/task1/HomeFragment.kt
@@ -5,21 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.task1.databinding.FragmentSecondBinding
+import com.example.task1.databinding.FragmentHomeBinding
 
 
-class SecondFragment : Fragment() {
+class HomeFragment : Fragment() {
 
-    private var _binding: FragmentSecondBinding? = null
+    private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/task1/LoginFragment.kt
+++ app/src/main/java/com/example/task1/LoginFragment.kt
@@ -5,15 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.replace
 import androidx.lifecycle.lifecycleScope
 import com.example.task1.databinding.FragmentLoginBinding
 import com.example.task1.models.StatusModel
 import com.example.task1.models.UserModel
 import com.example.task1.retrofit.LoginClientRetrofit
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+
 
 class LoginFragment : Fragment() {
+
 
     private var bind: FragmentLoginBinding? = null
 
@@ -24,7 +27,7 @@ class LoginFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
         bind = FragmentLoginBinding.inflate(inflater, container, false)
         return binding.root
@@ -35,39 +38,54 @@ class LoginFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val retrofit = LoginClientRetrofit()
-
-
+        val fragmentHome = HomeFragment()
         var user = UserModel()
+        var statusRetrive: StatusModel = StatusModel()
+        var statusLogin: StatusModel = StatusModel()
 
-        user.username = bind?.loginEmail.toString()
-        user.password = bind?.loginPass.toString()
+        binding.loginButton.setOnClickListener {
 
-        lifecycleScope.async(Dispatchers.IO) {
-            var status: StatusModel = StatusModel()
+            user.username = binding.loginEmail.editText?.text.toString()
+            user.password = binding.loginPass.editText?.text.toString()
 
-            //Request Token
-            status = retrofit.retrieveRequestToken()
+            lifecycleScope.launch(Dispatchers.IO) {
+                try {
+                    //Request Token
+                    statusRetrive = retrofit.retrieveRequestToken()
 
-            //Login
-            user.requestToken = status.requestToken
-            status = retrofit.login(user)
+                    //Login
+                    user.requestToken = statusRetrive.requestToken
+                    println("adsads")
+                    statusLogin = retrofit.login(user)
+                    println("dasdas")
 
-            //sessionId
-            status = retrofit.postSessionId(user.requestToken.toString())
-
-            //userDetailsA
-            user = retrofit.getUserDetails(status.sessionId.toString())
-
-            //invalidate session
-            status.success = retrofit.invalidateSession(status.sessionId.toString())
-
-            println(status)
+                    if (statusLogin.success == true) {
+                        parentFragmentManager.beginTransaction()
+                            .replace<HomeFragment>(R.id.fragment_container_view_tag)
+                            .commit()
+                    }
+                } catch (e: Exception) {
+                }
+            }
         }
+
+
+//            println("sgsa")
+//            //sessionId
+//            status = retrofit.postSessionId(user.requestToken.toString())
+//
+//            //userDetailsA
+//            user = retrofit.getUserDetails(status.sessionId.toString())
+//
+//            //invalidate session
+//            status.success = retrofit.invalidateSession(status.sessionId.toString())
+//
+//            println(status)
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
         bind = null
     }
-
 }
